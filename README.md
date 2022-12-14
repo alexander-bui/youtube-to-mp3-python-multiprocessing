1d5acfbda87352214d83a95ea1712d12b2938fca
alexander-bui <alexander.h.bui@outlook.com>
2022-12-14 17:51:38 -0600

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# youtube_to_mp3_python
-Download Video in MP3 format using PyTube
+# youtube_to_mp3_python_multiprocessing
+Download Video in MP3 format using PyTube.
+Ability to down to download multiple videos and playlists. Coded with multiprocessing for faster processing.
+
+## Prequesites
+Need to install Python 3. Recommended latest Python 3.
+
+## How to use
+1. (Optional): Create a virtual environment
+2. Run `pip install -r requirements.txt`
+3. Add youtube links to `youtube_urls` and  change destination folder in `destination`
+4. Adjust `PROCESS_TYPE` and `NUMBER_OF_WORKERS` to preference
+5. Run `python3 main.py` or `python main.py`
+
+## Example Output
+```
+2022-12-14 17:47:57,562 [INFO] Downloading videos
+2022-12-14 17:47:58,030 [INFO] Downloading Bella Donna.
+2022-12-14 17:47:58,157 [INFO] Downloading Blade Runner.
+2022-12-14 17:48:23,935 [INFO] Blade Runner has been successfully downloaded.
+2022-12-14 17:48:24,351 [INFO] Bella Donna has been successfully downloaded.
+2022-12-14 17:48:24,352 [INFO] Finished downloading videos
+```
---
+++ main.py
@@ -0,0 +1,68 @@
+
+# importing packages
+from pytube import YouTube, Playlist
+import os
+from multiprocessing import Pool, active_children
+from functools import partial
+
+PROCESS_TYPE = "URLS" # CHOOSE PLAYLIST or URLS
+NUMBER_OF_WORKERS = 4 # Declare num of core to use
+
+import logging
+logging.basicConfig(
+    level=logging.INFO, # INFO, ERROR
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+youtube_urls = [
+    "https://youtu.be/gVixaG76Ldg",
+    "https://youtu.be/Zx7K5wUYRSI",
+] # add more if needed
+
+p = Playlist('https://www.youtube.com/playlist?list=PLHTo__bpnlYUBJkury-RiqSizoXXmn082')
+
+destination = "C:/Music/"
+
+def download_mp3(link,download_type):
+    try:
+        # extract audio
+        if download_type == 1: # playlist
+            logging.info(f'Downloading {link.title}.')
+            audio = link.streams.get_audio_only()
+        else:
+            yt = YouTube(str(link))
+            logging.info(f'Downloading {yt.title}.')
+            audio = yt.streams.get_audio_only()
+            
+        # download the file
+        out_file = audio.download(output_path=destination) # 128kbps
+            
+        # save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = f'{base}.mp3'
+        os.rename(out_file, new_file)
+            
+        # result of success
+        if download_type == 1: # playlist
+            logging.info(f'{link.title} has been successfully downloaded.')
+        else:
+            logging.info(yt.title + " has been successfully downloaded.")
+    except Exception as e:
+        logging.error(f"error: {e}")
+
+
+if __name__ == '__main__':
+    pool = Pool(processes=NUMBER_OF_WORKERS)
+    try:
+        # create all tasks
+        logging.info(f"Downloading videos")
+        if PROCESS_TYPE == "PLAYLIST":
+            result = pool.map(partial(download_mp3, download_type=1), [link for link in p.videos])
+        else:
+            result = pool.map(partial(download_mp3, download_type=0), [link for link in youtube_urls])
+        logging.info(f"Finished downloading videos")
+    except KeyboardInterrupt:
+        # User interrupt the program with ctrl+c
+        logging.error(f"Exited via KeyboardInterrupt")
+        pool.terminate()
+        pool.join()
